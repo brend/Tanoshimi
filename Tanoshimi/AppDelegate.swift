5aa229de36ffdc071510adbd1d537c2f6ad36bfa
Philipp Brendel <waldrumpus@gmail.com>
2017-05-27 12:53:49 +0200

    Initial Commit

---
+++ Tanoshimi/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tanoshimi
+//
+//  Created by Philipp Brendel on 27.05.17.
+//  Copyright © 2017 Entenwolf Software. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tanoshimi/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tanoshimi
+//
+//  Created by Philipp Brendel on 27.05.17.
+//  Copyright © 2017 Entenwolf Software. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
